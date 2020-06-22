bdaee76ec42c6653f591b5f0b57fb8b65ef49d7c
BlasiusFosso <41558358+BlasGlobalCode@users.noreply.github.com>
2020-06-23 01:11:04 +0200

    Add files via upload

---
+++ Vorlage_ohneHilfe/WpfBindingTest1/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace WpfBindingTest1
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<BLand> GetBLands { get; set; }
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            GetBLands = new List<BLand>();
+            GetBLands.Add(new BLand("Rheinland", "Mainz",232));
+            this.DataContext = this;
+        }
+
+        public class BLand
+        {
+            public double Population = 2222;
+            public string l { get; set; }
+            public string bl { get; set; }
+
+            public BLand() { }
+            public BLand(string Land, string BundesLand, double Einwohner)
+            {
+                l = Land;
+                bl = BundesLand;
+                Population = Einwohner;
+            }
+
+            public override string ToString()
+            {
+                return bl;
+            }
+        }
+
+        private void btnEinfuegen_Click(object sender, RoutedEventArgs e)
+        {
+            Random rnd = new Random();
+            double a = 1000000;
+            a = rnd.NextDouble();
+            try
+            {
+                GetBLands.Add(new BLand(txtBland.Text,txtLand.Text,a));
+                txtBland.Text = string.Empty;
+                txtLand.Text = string.Empty;
+                LBoxLaender.Items.Refresh();
+            }
+            catch (Exception){}
+        }
+    }
+}
